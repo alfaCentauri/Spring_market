50a54b9ad3d4d8386d00432535c482fa53a8b0bf
Ricardo Presilla <ricardopresilla@gmail.com>
2022-12-29 09:24:41 -0300

    Add entitys and ProductRepository.

---
+++ src/main/java/com/alfaCentauri/onlineshop/domain/repository/ProductRepository.java
@@ -0,0 +1,16 @@
+package com.alfaCentauri.onlineshop.domain.repository;
+
+
+import com.alfaCentauri.onlineshop.domain.dto.Product;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductRepository {
+    List<Product> getAll();
+    Optional<List<Product>> getByCategory(int categoryId);
+    Optional<List<Product>> getScarseProduct(int quantity);
+    Optional<Product> getProduct(int poductId);
+    Product save(Product product);
+    void delete(int productId);
+}
